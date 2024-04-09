f4ffca827a381f654a350435f50d29b414475a79
amineElgaini <amineelgaini1444@gmail.com>
2024-04-09 15:31:25 +0000

    update borrow button in the borrowBook component

--- front/src/pages/BorrowBook/component/BorrowBookButton.jsx
+++ front/src/pages/BorrowBook/component/BorrowBookButton.jsx
@@ -13,13 +13,13 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { useGetUserByName } from "@/hooks/reactQuery/useUsers";
 import { useBorrowBook } from "@/hooks/reactQuery/useBorrowing";
-import LoadinLodingSpinner from "@/components/LodingSpinner";
+import { Loader2 } from "lucide-react";
 
 function BorrowBookButton({ bookId }) {
   const [username, setUsername] = useState("");
   const { data: user, isLoading } = useGetUserByName(username);
   const { mutate: borrowBook } = useBorrowBook();
-  
+
   const handleBorrow = () => {
     borrowBook({
       userId: user?.data?.data?.id,
@@ -39,50 +39,57 @@ function BorrowBookButton({ bookId }) {
         <DialogHeader>
           <DialogTitle>Select a User To Borrow Book [ {bookId} ]</DialogTitle>
           <DialogDescription className="flex flex-col">
-            <div className="mt-4 flex justify-center w-full max-w-sm items-center space-x-2">
+            <div className="mx-auto mt-4 flex justify-center w-full max-w-sm items-center space-x-2">
               <Input
                 onChange={(e) => setUsername(e.target.value)}
                 type="text"
-                placeholder="Usernae"
+                placeholder="Username"
               />
               <DialogClose
                 className=""
                 disabled={user?.data?.data?.id === undefined}
               >
                 <Button
-                  variant="green"
-                  disabled={user?.data?.data?.id === undefined}
                   onClick={() => {
                     handleBorrow();
                   }}
-                  size="sm"
+                  disabled={isLoading}
                 >
+                  {isLoading ? (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  ) : (
+                    ""
+                  )}
                   Borrow
                 </Button>
               </DialogClose>
             </div>
-            <div className="flex justify-between items-end">
-              <div className="mt-2 text-left text-base">
-                <p>
-                  <span className="dark:text-white text-black font-bold mr-2">
-                    UserId:
-                  </span>{" "}
-                  {user?.data?.data?.id}
-                </p>
-                <p>
-                  <span className="dark:text-white text-black font-bold mr-2">
-                    UserEmail:
-                  </span>{" "}
-                  {user?.data?.data?.email}
-                </p>
-                <p>
-                  <span className="dark:text-white text-black font-bold mr-2">
-                    UserName:
-                  </span>{" "}
-                  {user?.data?.data?.username}
-                </p>
-              </div>
-              <LoadinLodingSpinner isLoading={isLoading} />
+
+            <div className="mt-2 border-t border-gray-200 px-4 py-5 sm:p-0">
+              <dl className="sm:divide-y sm:divide-gray-200">
+                <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Username
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {user?.data?.data?.username}
+                  </dd>
+                </div>
+                <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">User Id</dt>
+                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {user?.data?.data?.id}
+                  </dd>
+                </div>
+                <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Email address
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {user?.data?.data?.email}
+                  </dd>
+                </div>
+              </dl>
             </div>
           </DialogDescription>
         </DialogHeader>
